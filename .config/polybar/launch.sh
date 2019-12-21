cac2699f65213af2a7d56c9d8626766e99fab058
Dylrex <dylrex@protonmail.com>
2019-12-21 19:55:58 +1100

    laptop compatability changes

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,8 +6,10 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch
-polybar main &
-polybar second &
-
+if [[ $(hostname) == 'somnia' ]]; then
+	polybar main &
+	polybar second &
+elif [[ $(hostname) == 'laptop' ]]; then
+	polybar laptop &
+fi
 echo "Polybar has been launched!"
